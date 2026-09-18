d165805f47e964de3d897f030a0c3d716cb1e308
Emre CRD <emrecirdi0@gmail.com>
2026-09-18 18:41:05 +0300

    adfcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,65 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';import Header from './components/layout/Header';import Footer from './components/layout/Footer';import Home from './pages/Home';import Generic from './pages/Generic';
-export default function App(){const pages=[['kurumsal','Kurumsal'],['yetkinlikler','Yetkinlikler'],['teknolojiler','Teknolojiler'],['urunler','Ürünler'],['ar-ge','Ar-Ge'],['medya','Medya'],['kariyer','Kariyer'],['iletisim','İletişim']];return <BrowserRouter><Header/><Routes><Route path="/" element={<Home/>}/>{pages.map(([p,t])=><Route key={p} path={'/'+p} element={<Generic title={t}/>}/>)}</Routes><Footer/></BrowserRouter>}
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import Generic from "./pages/Generic";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function AppRoutes() {
+  const pages = [
+    ["kurumsal", "Kurumsal"],
+    ["yetkinlikler", "Yetkinlikler"],
+    ["teknolojiler", "Teknolojiler"],
+    ["urunler", "Ürünler"],
+    ["ar-ge", "Ar-Ge"],
+    ["medya", "Medya"],
+    ["kariyer", "Kariyer"],
+    ["iletisim", "İletişim"],
+  ];
+
+  return (
+    <>
+      <ScrollToTop />
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {pages.map(([path, title]) => (
+          <Route
+            key={path}
+            path={`/${path}`}
+            element={<Generic title={title} />}
+          />
+        ))}
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
