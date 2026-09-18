5b06bdd3642e414d22a9f9e8ff9f5f432f62db8f
Emre CRD <emrecirdi0@gmail.com>
2026-09-19 00:36:27 +0300

    adfeaf

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,1 +1,1 @@
-export default function Container({children,className=''}){return <div className={`mx-auto w-full max-w-[1500px] px-5 md:px-10 xl:px-16 ${className}`}>{children}</div>}
+export default function Container({children,className=""}){return <div className={`mx-auto w-full max-w-[1540px] px-5 md:px-10 xl:px-16 ${className}`}>{children}</div>}
